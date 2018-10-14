d1647a9ae53af4136def4e1472eafac764b62227
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-10-14 04:20:01 -0400

    login form added

--- src/components/auth/Login/index.js
+++ src/components/auth/Login/index.js
@@ -1,12 +1,67 @@
-import React, { Component } from 'react'
-                               
+import React, { Component } from 'react';
+import classnames from 'classnames';
+
 class index extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      errors: {}
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  onChange(e) {this.setState({[e.target.name]:e.target.value})}
+  onSubmit(e) {
+    e.preventDefault();
+    const userData = {
+      email: this.state.email,
+      password:this.state.password
+    }
+    console.log(userData);
+  }
   render() {
+    const { errors } = this.state;
+
     return (
       <div className="container">
-        Login
+        <div className="row">
+          <div className="col-md-8 m-auto">
+            <h1 className="display-4 text-center">Log In</h1>
+            <p className="lead text-center">Sign in to your login account</p>
+
+            <form onSubmit={this.onSubmit}>
+              <div className="form-group">
+                <input
+                  type="email"
+                  name="email"
+                  className={classnames('form-control form-control-lg', {
+                    'is-valid': errors.email
+                  })}
+                  value={this.state.email}
+                  onChange={this.onChange}
+                  placeholder="Email Address"
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="password"
+                  name="password"
+                  className={classnames('form-control form-control-lg', {
+                    'is-valid': errors.password
+                  })}
+                  value={this.state.password}
+                  onChange={this.onChange}
+                  placeholder="Password Address"
+                />
+              </div>
+              <input type="submit" className="btn btn-info btn-block mt-4" />
+            </form>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
-export default index
+export default index;
--- src/components/header_footer/Footer/index.js
+++ src/components/header_footer/Footer/index.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react'
 class index extends Component {
   render() {
     return (
-      <div>
+      <footer className="bg-dark text-white mt-5  p-4 text-center">
       Footer works
-      </div>
+      </footer>
     )
   }
 }
